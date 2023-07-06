301e274110c18a56213003dddc0dc2d715a5271a
Magic <730170034@qq.com>
2023-07-06 15:30:56 +0800

    Initial Commit

---
+++ bmob-chatgpt-demo/bmob_chatgpt_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  bmob_chatgpt_demoApp.swift
+//  bmob-chatgpt-demo
+//
+//  Created by magic on 2023/7/6.
+//
+
+import SwiftUI
+
+@main
+struct bmob_chatgpt_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bmob-chatgpt-demoUITests/bmob_chatgpt_demoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  bmob_chatgpt_demoUITestsLaunchTests.swift
+//  bmob-chatgpt-demoUITests
+//
+//  Created by magic on 2023/7/6.
+//
+
+import XCTest
+
+final class bmob_chatgpt_demoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
